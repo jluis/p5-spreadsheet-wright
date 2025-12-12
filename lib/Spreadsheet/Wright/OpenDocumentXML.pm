3ae91f914b970a99dad2b212fd20617ddd9f7201
Jose Luis Perez <jluis@escomposlinux.org>
2025-12-12 18:26:38 +0100

    put dates times and numbers as is types

--- lib/Spreadsheet/Wright/OpenDocumentXML.pm
+++ lib/Spreadsheet/Wright/OpenDocumentXML.pm
@@ -21,6 +21,7 @@ use constant {
 	TABLE_NS  => "urn:oasis:names:tc:opendocument:xmlns:table:1.0",
 	META_NS   => "urn:oasis:names:tc:opendocument:xmlns:meta:1.0",
 	NUMBER_NS => "urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0",
+	CALCEXT_NS => "urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0",
 	};
 
 sub new
@@ -55,6 +56,7 @@ sub _prepare
 		table   => TABLE_NS,
 		meta    => META_NS,
 		number  => NUMBER_NS,
+		calcext => CALCEXT_NS,
 		};
 	
 	$self->{'document'} = XML::LibXML->createDocument;
@@ -67,6 +69,51 @@ sub _prepare
 	}
 	$self->{'document'}->documentElement->setNodeName('office:document-content');
 	$self->{'document'}->documentElement->setAttributeNS(OFFICE_NS, 'version', '1.0');
+	###### styles
+	my $automatic_styles = $self->{'document'}->documentElement
+		->addNewChild(OFFICE_NS, 'automatic-styles');
+	my $date_style = $automatic_styles->addNewChild(NUMBER_NS, 'date-style');
+	$date_style->setAttributeNS(STYLE_NS,'name','N84');
+	$date_style->addNewChild(NUMBER_NS,'year')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text')->appendText('-');
+	$date_style->addNewChild(NUMBER_NS,'month')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text')->appendText('-');
+	$date_style->addNewChild(NUMBER_NS,'day')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style = $automatic_styles->addNewChild(NUMBER_NS, 'date-style');
+	$date_style->setAttributeNS(STYLE_NS,'name','N52');
+	$date_style->addNewChild(NUMBER_NS,'year')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text')->appendText('-');
+	$date_style->addNewChild(NUMBER_NS,'month')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text')->appendText('-');
+	$date_style->addNewChild(NUMBER_NS,'day')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text');
+	$date_style->addNewChild(NUMBER_NS,'hours')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text')->appendText(':');
+	$date_style->addNewChild(NUMBER_NS,'minutes')->setAttributeNS(NUMBER_NS,'style','long');
+	$date_style->addNewChild(NUMBER_NS,'text')->appendText(':');
+	$date_style->addNewChild(NUMBER_NS,'seconds')->setAttributeNS(NUMBER_NS,'style','long');
+	my $time_style = $automatic_styles->addNewChild(NUMBER_NS, 'time-style');
+	$time_style->setAttributeNS(STYLE_NS,'name','N41');
+	$time_style->addNewChild(NUMBER_NS,'hours')->setAttributeNS(NUMBER_NS,'style','long');
+	$time_style->addNewChild(NUMBER_NS,'text')->appendText(':');
+	$time_style->addNewChild(NUMBER_NS,'minutes')->setAttributeNS(NUMBER_NS,'style','long');
+	$time_style->addNewChild(NUMBER_NS,'text')->appendText(':');
+	$time_style->addNewChild(NUMBER_NS,'seconds')->setAttributeNS(NUMBER_NS,'style','long');
+	my $ce1 = $automatic_styles->addNewChild(STYLE_NS,'style');
+	$ce1->setAttributeNS(STYLE_NS,'name','ce1');
+	$ce1->setAttributeNS(STYLE_NS,'family','table-cell');
+	$ce1->setAttributeNS(STYLE_NS,'parent-style-name','Default');
+	$ce1->setAttributeNS(STYLE_NS,'data-style-name','N84');
+	my $ce5 = $automatic_styles->addNewChild(STYLE_NS,'style');
+	$ce5->setAttributeNS(STYLE_NS,'name','ce5');
+	$ce5->setAttributeNS(STYLE_NS,'family','table-cell');
+	$ce5->setAttributeNS(STYLE_NS,'parent-style-name','Default');
+	$ce5->setAttributeNS(STYLE_NS,'data-style-name','N41');
+	my $ce7 = $automatic_styles->addNewChild(STYLE_NS,'style');
+	$ce7->setAttributeNS(STYLE_NS,'name','ce7');
+	$ce7->setAttributeNS(STYLE_NS,'family','table-cell');
+	$ce7->setAttributeNS(STYLE_NS,'parent-style-name','Default');
+	$ce7->setAttributeNS(STYLE_NS,'data-style-name','N52');
 	$self->{'body'} = $self->{'document'}->documentElement
 		->addNewChild(OFFICE_NS, 'body')
 		->addNewChild(OFFICE_NS, 'spreadsheet');
@@ -100,34 +147,40 @@ sub _add_prepared_row
 	foreach my $cell (@_)
 	{
 		my $tcell = $tr->addNewChild(TABLE_NS, 'table-cell');
-		if (defined $cell->{type}) {
-			my $type = $cell->{type};
-			if ($type eq 'date') {
-				my $value = ( $cell->{content} =~ m|^(\d\d)/(\d\d)/(\d\d\d\d)$| ) ? "$3-$2-$1" : ($cell->{content} =~ /^\d\d\d\d-\d\d-\d\d$/) ?$ &:carp "invalid date value";
-				$tcell->setAttributeNS(OFFICE_NS, 'value-type',$type);
-				$tcell->setAttributeNS(OFFICE_NS, 'date-value',$value);
-			}
-			elsif ($type eq 'float') {
-				...
-			}
-			elsif ($type eq 'percentage'){
-				...
-			}
-			elsif ($type eq 'currency'){
-				...
-			}
-			elsif ($type eq 'time' ) {
-				...
-			}
-			elsif ($type eq 'boolean') {
-				...
-			}
-			elsif ($type eq 'string'){
-				$tcell->setAttributeNS(OFFICE_NS, 'value-type', $type )
-			}
-			else {
-				carp "Unknown cell type $type"
-			}
+		if ($cell->{content} =~ /^\d\d\d\d-\d\d-\d\d$/) {
+			$tcell->setAttributeNS(OFFICE_NS, 'date-value',$cell->{content});
+			$tcell->setAttributeNS(OFFICE_NS, 'value-type','date');
+			$tcell->setAttributeNS(CALCEXT_NS, 'value-type','date');
+			$tcell->setAttributeNS(TABLE_NS,'style-name','ce1');
+		}
+		elsif ($cell->{content} =~ /^(\d\d):(\d\d):(\d\d)$/) {
+			$tcell->setAttributeNS(OFFICE_NS, 'time-value',"PT$1H$2M$3S");
+			$tcell->setAttributeNS(OFFICE_NS, 'value-type','time');
+			$tcell->setAttributeNS(CALCEXT_NS, 'value-type','time');
+			$tcell->setAttributeNS(TABLE_NS,'style-name','ce5');
+		}
+		elsif ($cell->{content} =~ /^(\d\d\d\d-\d\d-\d\d) (\d\d:\d\d:\d\d)$/) {
+			$tcell->setAttributeNS(OFFICE_NS, 'date-value',"$1T$2");
+			$tcell->setAttributeNS(OFFICE_NS, 'value-type','date');
+			$tcell->setAttributeNS(CALCEXT_NS, 'value-type','date');
+			$tcell->setAttributeNS(TABLE_NS,'style-name','ce7');
+		}
+		elsif ($cell->{content} =~ /^(?:(?i)(?:[-+]?)(?:(?=[.]?[0123456789])(?:[0123456789]{1,3}(?:(?:,?)[0123456789]{3})*)(?:(?:[.])(?:[0123456789]{0,}))?))$/ ) {
+		#float English format
+		my $value =  $cell->{content} =~ s/,//gr;
+			$tcell->setAttributeNS(OFFICE_NS, 'value',$value);
+			$tcell->setAttributeNS(OFFICE_NS, 'value-type','float');
+			$tcell->setAttributeNS(CALCEXT_NS, 'value-type','float');
+			$tcell->setAttributeNS(TABLE_NS,'style-name','Default');
+		}
+		elsif ($cell->{content} =~ /^(?:(?i)(?:[-+]?)(?:(?=[,]?[0123456789])(?:[0123456789]{1,3}(?:(?:\.?)[0123456789]{3})*)(?:(?:[,])(?:[0123456789]{0,}))?))$/ ) {
+		#float spanish format
+		my $value =  $cell->{content} =~ s/[.]//gr;
+		$value =~ s/,/./;
+			$tcell->setAttributeNS(OFFICE_NS, 'value',$value);
+			$tcell->setAttributeNS(OFFICE_NS, 'value-type','float');
+			$tcell->setAttributeNS(CALCEXT_NS, 'value-type','float');
+			$tcell->setAttributeNS(TABLE_NS,'style-name','Default');
 		}
 		else {
 			$tcell->setAttributeNS(OFFICE_NS,'value-type','string')
